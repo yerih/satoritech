21a6817fe30b0b3e44255791fc530b923805ccc7
yerih <yerih7tup@gmail.com>
2023-09-06 18:38:15 -0400

    Fix error. In MainNavigation, modifier argument doesn't exist for ModelScreen

--- app/src/main/java/android/template/ui/Navigation.kt
+++ app/src/main/java/android/template/ui/Navigation.kt
@@ -30,7 +30,7 @@ fun MainNavigation() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "main") {
-        composable("main") { MyModelScreen(modifier = Modifier.padding(16.dp)) }
+        composable("main") { MyModelScreen() }
         // TODO: Add more destinations
     }
 }
